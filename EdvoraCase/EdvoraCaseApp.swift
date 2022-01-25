8129bed303988ae14032f946a947e2b87f442812
Ertan <eguner16@ku.edu.tr>
2022-01-25 21:45:17 +0300

    Initial Commit

---
+++ EdvoraCase/EdvoraCaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdvoraCaseApp.swift
+//  EdvoraCase
+//
+//  Created by Ertan Can Güner on 25.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct EdvoraCaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
